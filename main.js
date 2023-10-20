f6a414c979b93365149f025424a413b2a3218505
rebekkarutm <rmdottir@gmail.com>
2023-10-20 11:10:42 +0000

    forgot to watch typescript for changes

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 "use strict";
 fetch('https://rickandmortyapi.com/api/character').then((r) => r.json()).then((json) => {
+    // creating a forEach loop that returns all of the information I want in one log
     json.results.forEach((character) => {
         console.log(`Name: ${character.name} ||| ` + `Origin: ${character.origin.name} ||| ` + `Species: ${character.species} ||| ` + `Number of episodes: ${character.episode.length}`);
     });
